ab5bf273b27743147245b3a126e387625121a929
Basil-Shitemi <basilshitemi@gmail.com>
2025-06-25 23:19:37 +0300

    codes

---
+++ intro.py
@@ -0,0 +1,26 @@
+#str
+print('Hello World')
+name = input('what is your name?')
+print('Hello ' + name)
+
+#fundamental data types
+## int and float
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(4 / 2)
+print(type(2 / 4))
+print(4 // 2)
+print(type(4 // 2))
+print(type(2 + 4))
+print(type(2.1 + 4))
+print(6 % 4)
+print(2**3)
+
+#math functions(Google python3 math functions)
+print(round(3.1))
+print(abs(-20))
+#Operator precedence(obeys BODMAS)
+print(20-3*4)
+print(bin(5))
+print(int('0b101', 2))
